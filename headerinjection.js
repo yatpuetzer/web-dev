b7a7deb9a337fdd162e5fe669140e6c466c27164
yatpuetzer <student.tp@youngsvilleacademy.org>
2026-04-27 13:02:03 -0400

    Update headerinjection.js

--- headerinjection.js
+++ headerinjection.js
@@ -1,5 +1,5 @@
 let elem = document.createElement("div");
-elem.innerhtml = "<link rel='stylesheet' href='https://yatpuetzer.github.io/web-dev/wdsub/journal.css'>";
+elem.innerhtml = "<link rel='stylesheet' href='https://yatpuetzer.github.io/web-dev/header.css'>";
 document.body.prepend(elem);
 
 fetch("https://yatpuetzer.github.io/web-dev/globalheader.html")
